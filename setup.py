1fc7d0a10834ade17f27568f4de0860fd86014ee
Nikesh Raj Wagle <nikeshwagle30@gmail.com>
2025-08-29 11:50:03 +0545

    exception and logger

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 from typing import List
 
 
-def get_requirements()->list[str]:
-    requirements_list=list[str]=[]
+def get_requirements()->List[str]:
+    requirements_list : List[str]=[]
     return requirements_list
 
 
